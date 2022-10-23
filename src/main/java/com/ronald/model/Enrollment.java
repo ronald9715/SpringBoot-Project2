3c5b4404089af64782fc448e042e3980e842fdf8
Ronald <ronald.agama@unmsm.edu.pe>
2022-10-23 18:52:57 -0500

    Tablas Maestro-Detalle

---
+++ src/main/java/com/ronald/model/Enrollment.java
@@ -0,0 +1,35 @@
+package com.ronald.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//JPA
+@Entity
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idEnrollment;
+    @Column(nullable = false)
+    private LocalDateTime localDateTime;
+    //Establecer una relacion de ONE TO ONE
+    //Se utiliza el JoinColumn para relacionar las tablas
+    @OneToOne
+    @JoinColumn(name = "id_student", nullable = false, foreignKey = @ForeignKey(name = "FK_Enrollment_Student"))
+    private Student student;
+    //Relacion Logica Maestro - Detalle
+    //El mappedBy debe tener el nombre que tiene la clase EnrollmentDetail
+    @OneToMany(mappedBy = "enrollment", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<EnrollmentDetail> details;
+    @Column(nullable = false)
+    private boolean status;
+}
---
+++ src/main/java/com/ronald/model/EnrollmentDetail.java
@@ -0,0 +1,30 @@
+package com.ronald.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//JPA
+@Entity
+public class EnrollmentDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idEnrollmentDetail;
+    //Debe tener una relacion con Enrollment
+    //Muchos detalles pueden pertenecer a una matricula
+    @ManyToOne
+    @JoinColumn(name = "id_enrollment", nullable = false)
+    private Enrollment enrollment;
+    @ManyToOne
+    @JoinColumn(name = "id_subject", nullable = false, foreignKey = @ForeignKey(name = "FK_EnrollmentDT_Subject"))
+    private Subject subject;
+    @Column(length = 10, nullable = false)
+    private String room;
+}
